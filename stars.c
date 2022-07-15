002ad1763a3650572b406767bcedb0fd2b188d96
TANYA SHANKAR <89242225+tanyasnu09@users.noreply.github.com>
2022-07-15 19:28:41 +0530

    Update stars.c

--- stars.c
+++ stars.c
@@ -1,3 +1,8 @@
+//*
+**
+***
+****
+
 #include <stdio.h>
 #include<stdlib.h>
 int main()
@@ -5,7 +10,7 @@ int main()
     int i,j;
     for (i=0;i<5;i++)
     {
-        for(j=0;j<5;j++)
+        for(j=0;j<i;j++)
         {
             printf("*");
         }
